6889f806cb43d630814e7475bf7d5b3be5c94630
SadHappy <craigjm19@gmail.com>
2024-08-12 18:00:25 +0100

    Added a basic Modal with JS, CSS behaviour and styling

---
+++ scripts/build/modal.js
@@ -0,0 +1,16 @@
+"use strict";
+var modal = document.getElementById("modal-container");
+var aboutButton = document.getElementById("modal-button");
+var modalClose = document.getElementsByClassName("modal-close")[0];
+aboutButton.onclick = function () {
+    modal.style.display = "block";
+};
+modalClose.onclick = function () {
+    modal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
---
+++ scripts/modal.ts
@@ -0,0 +1,18 @@
+var modal = document.getElementById("modal-container") as HTMLElement;
+var aboutButton = document.getElementById("modal-button") as HTMLButtonElement;
+var modalClose = document.getElementsByClassName("modal-close")[0] as HTMLSpanElement;
+
+aboutButton.onclick = function() {
+    modal.style.display = "block";
+}
+
+modalClose.onclick = function() {
+	modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
