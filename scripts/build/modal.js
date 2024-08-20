782222a6ef033fbcde18bb848b11ab0ce03e26bd
SadHappy <craigjm19@gmail.com>
2024-08-20 12:08:09 +0100

    Added ability to open and close 2 modals

--- scripts/build/modal.js
+++ scripts/build/modal.js
@@ -1,16 +1,30 @@
 "use strict";
-var modal = document.getElementById("modal-container");
+var settingsModal = document.getElementById("settings-modal");
+var aboutModal = document.getElementById("about-modal");
+var settingsButton = document.getElementById("settings-button");
 var aboutButton = document.getElementById("info-button");
-var modalClose = document.getElementsByClassName("modal-close")[0];
-aboutButton.onclick = function () {
-    modal.style.display = "block";
+var settingsClose = settingsModal.getElementsByClassName("modal-close")[0];
+var aboutClose = aboutModal.getElementsByClassName("modal-close")[0];
+// Mouse click
+settingsButton.onclick = function () {
+    settingsModal.style.display = "block";
 };
-modalClose.onclick = function () {
-    modal.style.display = "none";
+aboutButton.onclick = function () {
+    aboutModal.style.display = "block";
+};
+// Close buttons
+settingsClose.onclick = function () {
+    settingsModal.style.display = "none";
+};
+aboutClose.onclick = function () {
+    aboutModal.style.display = "none";
 };
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == settingsModal) {
+        settingsModal.style.display = "none";
+    }
+    else if (event.target == aboutModal) {
+        aboutModal.style.display = "none";
     }
 };
--- scripts/modal.ts
+++ scripts/modal.ts
@@ -1,18 +1,37 @@
-var modal = document.getElementById("modal-container") as HTMLElement;
-var aboutButton = document.getElementById("info-button") as HTMLButtonElement;
-var modalClose = document.getElementsByClassName("modal-close")[0] as HTMLSpanElement;
+var settingsModal = document.getElementById("settings-modal") as HTMLElement;
+var aboutModal = document.getElementById("about-modal") as HTMLElement;
 
-aboutButton.onclick = function() {
-    modal.style.display = "block";
+var settingsButton = document.getElementById("settings-button") as HTMLButtonElement;
+var aboutButton = document.getElementById("info-button") as HTMLButtonElement;
+
+var settingsClose = settingsModal.getElementsByClassName("modal-close")[0] as HTMLSpanElement;
+var aboutClose = aboutModal.getElementsByClassName("modal-close")[0] as HTMLSpanElement;
+
+// Mouse click
+settingsButton.onclick = function() {
+    settingsModal.style.display = "block";
 }
 
-modalClose.onclick = function() {
-	modal.style.display = "none";
+aboutButton.onclick = function() {
+    aboutModal.style.display = "block";
+}
+
+// Close buttons
+settingsClose.onclick = function() {
+	settingsModal.style.display = "none";
+}
+
+aboutClose.onclick = function() {
+	aboutModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
+	if (event.target == settingsModal) {
+		settingsModal.style.display = "none";
+	}
+
+	else if (event.target == aboutModal) {
+		aboutModal.style.display = "none";
 	}
 }
